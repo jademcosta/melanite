6b8ed19614ceddf994d81bb705769c53479f725e
jademcosta <jademcosta@gmail.com>
2017-07-26 16:52:51 -0300

    Use tests setup/teardown and use dummy images server
    With this, we can hit a real server to ask for images and work with it
    on tests.

--- main_test.go
+++ main_test.go
@@ -8,75 +8,35 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/julienschmidt/httprouter"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-// func TestMain(m *testing.M) {
-//     a = main.App{}
-//     a.Initialize(
-//         os.Getenv("TEST_DB_USERNAME"),
-//         os.Getenv("TEST_DB_PASSWORD"),
-//         os.Getenv("TEST_DB_NAME"))
-//
-//     ensureTableExists()
-//
-//     code := m.Run()
-//
-//     clearTable()
-//
-//     os.Exit(codunc TestMain(m *testing.M) {
-//     a = main.App{}
-//     a.Initialize(
-//         os.Getenv("TEST_DB_USERNAME"),
-//         os.Getenv("TEST_DB_PASSWORD"),
-//         os.Getenv("TEST_DB_NAME"))
-//
-//     ensureTableExists()
-//
-//     code := m.Run()
-//
-//     clearTable()
-//
-//     os.Exit(code)
-// }e)
-// }
-
-func TestSimple(t *testing.T) {
-	req, _ := http.NewRequest("GET", "/doesntmatter:(", nil)
-	response := httptest.NewRecorder()
-
-	oi := []httprouter.Param{httprouter.Param{Key: "file", Value: "jade"}}
-
-	FetcherFunc(response, req, oi)
-
-	assert.Equal(t, response.Body.String(), "Hello jade\n", "they should be equal")
+type FakeExternalServerTestSuite struct {
+	suite.Suite
+	fakeServer    *http.Server
+	subjectServer *httptest.Server
 }
 
-func TestFromOutside(t *testing.T) {
-	ts := httptest.NewServer(GetApp())
-	defer ts.Close()
-
-	res, err := http.Get(fmt.Sprintf("%s/%s", ts.URL, "jade"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	greeting, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	assert.Equal(t, 200, res.StatusCode, "status code should be 200")
-	assert.Equal(t, string(greeting), "Hello jade\n", "they should be equal")
+func (suite *FakeExternalServerTestSuite) SetupSuite() {
+	suite.fakeServer = &http.Server{Addr: ":8081", Handler: http.FileServer(http.Dir("."))}
+	go func() {
+		suite.fakeServer.ListenAndServe()
+		// http.ListenAndServe(":8081", suite.fakeServer)
+	}()
 }
 
-func TestNoUrlReturnsBadRequest(t *testing.T) {
-	ts := httptest.NewServer(GetApp())
-	defer ts.Close()
+func TestFakeExternalServerTestSuite(t *testing.T) {
+	suite.Run(t, new(FakeExternalServerTestSuite))
+}
 
-	res, err := http.Get(fmt.Sprintf("%s/%s", ts.URL, ""))
+func (suite *FakeExternalServerTestSuite) SetupTest() {
+	suite.subjectServer = httptest.NewServer(GetApp())
+	// defer suite.subjectServer.Close()
+}
+
+func (suite *FakeExternalServerTestSuite) TestExample() {
+	res, err := http.Get(fmt.Sprintf("%s/%s",
+		suite.subjectServer.URL, "http://localhost:8081/oi.txt"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -87,6 +47,16 @@ func TestNoUrlReturnsBadRequest(t *testing.T) {
 		log.Fatal(err)
 	}
 
-	assert.Equal(t, 400, res.StatusCode, "status code should be 400")
-	assert.Equal(t, string(body), "No image url provided\n", "they should be equal")
+	suite.Equal(404, res.StatusCode, "status code should be 404")
+	suite.Equal(string(body), "\n", "they should be equal")
+}
+
+func (suite *FakeExternalServerTestSuite) TearDownTest() {
+	suite.subjectServer.Close()
+}
+
+func (suite *FakeExternalServerTestSuite) TearDownSuite() {
+	if err := suite.fakeServer.Shutdown(nil); err != nil {
+		panic(err)
+	}
 }
