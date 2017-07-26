e7014dd42710d06bf6adbf8f0c7c4c9373edbb56
jademcosta <jademcosta@gmail.com>
2017-07-26 16:54:47 -0300

    Enhance errors when fetching images

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"io"
 	"net/http"
 	"strings"
 
@@ -14,19 +15,73 @@ func main() {
 
 func GetApp() http.Handler {
 	r := httprouter.New()
-	r.GET("/*file", FetcherFunc)
+	r.GET("/*fileUri", FetcherFunc)
 	return r
 }
 
 func FetcherFunc(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	filename := removePrefixSlash(p.ByName("file"))
+	url := removePrefixSlash(p.ByName("fileUri"))
 
-	if filename == "" {
-		http.Error(rw, "No image url provided", http.StatusBadRequest)
+	if !isValid(url) {
+		http.Error(rw, "Invalid image url provided", http.StatusBadRequest)
 		return
 	}
 
-	fmt.Fprintln(rw, fmt.Sprintf("Hello %s", filename))
+	response, err := getImage(&url)
+	if err != nil {
+		http.Error(rw, "", http.StatusInternalServerError)
+		return
+	}
+
+	if externalImageNotFound(response) {
+		http.Error(rw, "", http.StatusNotFound)
+		return
+	}
+
+	defer response.Body.Close()
+
+	imgAsBytes, err := decodeImageFromBody(&response.Body)
+	if err != nil {
+		http.Error(rw, "", http.StatusInternalServerError)
+		return
+	}
+
+	format := response.Header.Get("Content-type")
+
+	if len(format) == 0 {
+		http.Error(rw, "Content-type not defined by upstream server", http.StatusInternalServerError)
+		return
+	}
+
+	rw.Header().Add("Content-type", format)
+	rw.Write(*imgAsBytes)
+}
+
+func decodeImageFromBody(body *io.ReadCloser) (*[]byte, error) {
+	buf := &bytes.Buffer{}
+	_, err := buf.ReadFrom(*body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	b := buf.Bytes()
+	return &b, nil
+}
+
+func externalImageNotFound(response *http.Response) bool {
+	return response.StatusCode == http.StatusNotFound
+}
+
+func getImage(url *string) (*http.Response, error) {
+	return http.Get(*url)
+}
+
+func isValid(s string) bool {
+	if s == "" {
+		return false
+	}
+	return true
 }
 
 func removePrefixSlash(s string) string {
